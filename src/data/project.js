5cae748c5f1f19f3aea2c5925a504b7a8b32954e
Shubham75511 <111749753+Shubham75511@users.noreply.github.com>
2025-06-25 17:07:43 +0530

    Update project.js

--- src/data/project.js
+++ src/data/project.js
@@ -2,32 +2,32 @@ import project from "../assets/projects/project.svg";
 
 const projects = [
   {
+    name: "Real Estate Website",
+    img: project,
+    description:
+      "Built a responsive real estate platform using React.js, Node.js, Express.js, and MongoDB for listing and managing properties.Developed property search, filter, and sort features based on location, price, property type, and amenities.Implemented user authentication (JWT/OAuth) for secure user login, registration, and role-based access (agents, buyers). Designed responsive UI/UX with Tailwind CSS/Bootstrap to ensure cross-device compatibility.",
+    link: "https://github.com/Shubham75511/Real-Estate-App",
+  },
+  {
     name: "Keeper App",
     img: project,
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-    link: "https://github.com",
-  },
-  {
-    name: "To-Do App",
-    img: project,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-    link: "https://github.com",
+      "Added the features to dynamically Add and Delete Notes by passing required parameters to each of the components. Developed the application using Hooks and States in ReactJS. Created separate components for notes, input area, header, footer.",
+    link: "https://github.com/Shubham75511/Keeper-App",
   },
   {
     name: "TinDog Website",
     img: project,
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-    link: "https://github.com",
+      "Built using Bootstrap to add different functionalities. Used CSS to style the website.",
+    link: "https://github.com/Shubham75511/tindog-website",
   },
   {
-    name: "Lorem Ipsum",
+    name: "To-Do Website",
     img: project,
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-    link: "https://github.com",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    link: "https://github.com/Shubham75511/React-ToDoListApp",
   },
 ];
 
